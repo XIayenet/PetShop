3bc774fd4bceb9c167e34ac7663c110af12b59d2
Mmotkim <mmotkim2@gmail.com>
2025-03-12 16:16:40 +0700

    first page

--- src/java/controller/LandingPage.java
+++ src/java/controller/LandingPage.java
@@ -29,18 +29,7 @@ public class LandingPage extends HttpServlet {
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
-        try (PrintWriter out = response.getWriter()) {
-            /* TODO output your page here. You may use following sample code. */
-            out.println("<!DOCTYPE html>");
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<title>Servlet LandingPage</title>");
-            out.println("</head>");
-            out.println("<body>");
-            out.println("<h1>Servlet LandingPage at " + request.getContextPath() + "</h1>");
-            out.println("</body>");
-            out.println("</html>");
-        }
+        
     }
 
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
@@ -56,6 +45,7 @@ public class LandingPage extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         processRequest(request, response);
+        response.sendRedirect("index.jsp");
     }
 
     /**
