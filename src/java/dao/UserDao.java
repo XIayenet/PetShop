4c03af121ce0f9d661306f1610a7b0f4deccb4e0
Xiayenet <160136953+XIayenet@users.noreply.github.com>
2025-03-12 19:34:54 +0700

    userDao

--- src/java/dao/UserDao.java
+++ src/java/dao/UserDao.java
@@ -6,47 +6,60 @@
 package dao;
 
 import java.sql.ResultSet;
+import entity.UserEntity;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
-import com.sun.jdi.connect.spi.Connection;
-import java.sql.PreparedStatement;/**
- * @author admin
- */
+import java.util.ArrayList;
+
 public class UserDao extends DBContext  {
+    
+     public UserDao() {}
+     
   public void createUser(String username, String password, String role) throws SQLException {
         String sql = "INSERT INTO [User] (Username, Pass, Role) VALUES (?, ?, ?)";
-        try (PreparedStatement ps = DBConnection.getConnection();
-            PreparedStatement stmt = ps.prepareStatement(sql)) {
-            stmt.setString(1, username);
-            stmt.setString(2, password);
-            stmt.setString(3, role);
-            stmt.executeUpdate();
-        }
+        try 
+        {
+            PreparedStatement ps = connection.prepareStatement(sql);
+            ps.setString(1, username);
+            ps.setString(2, password);
+            ps.setString(3, role);
+            ps.executeUpdate();
+        }catch (SQLException e) {
+    }
     }
     
     public ResultSet getUser(int userId) throws SQLException {
         String sql = "SELECT * FROM [User] WHERE UserID = ?";
-        PreparedStatement conn = DBConnection.getConnection();
-        PreparedStatement stmt = conn.prepareStatement(sql);
-        stmt.setInt(1, userId);
-        return stmt.executeQuery();
+        PreparedStatement ps = connection.prepareStatement(sql);
+        ps.setInt(1, userId);
+        return ps.executeQuery();
     }
     
     public void updateUser(int userId, String username, String password, String role) throws SQLException {
         String sql = "UPDATE [User] SET Username = ?, Pass = ?, Role = ? WHERE UserID = ?";
-        try (PreparedStatement conn = DBConnection.getConnection(); PreparedStatement stmt = conn.prepareStatement(sql)) {
-            stmt.setString(1, username);
-            stmt.setString(2, password);
-            stmt.setString(3, role);
-            stmt.setInt(4, userId);
-            stmt.executeUpdate();
+        try 
+             
+        {
+            PreparedStatement ps = connection.prepareStatement(sql);
+            ps.setString(1, username);
+            ps.setString(2, password);
+            ps.setString(3, role);
+            ps.setInt(4, userId);
+            ps.executeUpdate();
+        }catch(Exception e){
+            
         }
+            
     }
     
     public void deleteUser(int userId) throws SQLException {
         String sql = "DELETE FROM [User] WHERE UserID = ?";
-        try (PreparedStatement conn = DBConnection.getConnection(); PreparedStatement stmt = conn.prepareStatement(sql)) {
-            stmt.setInt(1, userId);
-            stmt.executeUpdate();
+        try  {
+            PreparedStatement ps = connection.prepareStatement(sql);
+            ps.setInt(1, userId);
+            ps.executeUpdate();
+        }catch(Exception e){
+            
         }
     }
 
